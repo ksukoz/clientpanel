a5bf5bee7f467256468b563803341b8170c23dd5
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-05-14 16:13:03 +0300

    Connect to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// AngularFire imports
+import { AngularFireModule } from 'angularfire2'; 
+import { AngularFireDatabase } from 'angularfire2/database'; 
+import { AngularFireAuth } from 'angularfire2/auth'; 
+
+// Component imports
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ClientsComponent } from './components/clients/clients.component';
@@ -15,11 +21,22 @@ import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+// Service imports
+import { ClientService } from './services/client.service';
+
 const appRoutes: Routes = [
   {path: '', component: DashboardComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent}
 ];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBEGcDKQ0HbDnUQHiltemZSSur7Hp_FOTM",
+  authDomain: "clientpanel-c6664.firebaseapp.com",
+  databaseURL: "https://clientpanel-c6664.firebaseio.com",
+  storageBucket: "clientpanel-c6664.appspot.com",
+  messagingSenderId: "163221156191"
+};
 
 @NgModule({
   declarations: [
@@ -38,9 +55,14 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
